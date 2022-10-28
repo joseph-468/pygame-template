87506d5308b7a33436fa0089a7f7f7bac00ba34d
joseph-468 <75801386+joseph-468@users.noreply.github.com>
2022-10-28 18:13:37 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,68 +1,35 @@
-# Essential
 import pygame
-import os
-
-# Very useful
-import math
-import random
-
-# Colors
-BLACK = 0, 0, 0
-WHITE = 255, 255, 255
-RED = 255, 0, 0
-LIME = 0, 255, 0
-BLUE = 0, 0, 255
-YELLOW = 255, 255, 0
-CYAN = 0, 255, 255
-MAGENTA = 255, 0, 255
-SILVER = 192, 192, 192
-GREY = 128, 128, 128
-DARKGREY = 64, 64, 64
-MAROON = 128, 0, 0
-OLIVE = 128, 128, 0
-GREEN = 0, 128, 0
-PURPLE = 128, 0, 128
-TEAL = 0, 128, 128
-NAVY = 0, 0, 128
 
 # CONSTANTS
-WIDTH, HEIGHT = 800, 800  # Resolution of screen
-FPS = 60  # How many times the game should update per second
-
-# Variables
-
-# Setup
-pygame.init()
-pygame.mixer.init()
-pygame.font.init()
-pygame.display.set_caption("ExampleName")
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
-# Loads assets
-
-# Functions
+WIDTH, HEIGHT = 800, 800
+FPS = 60
 
 
-# Main loop
+# Main
 def main():
-    # Variables used in game
-    run = True
+    # Setup
+    pygame.init()
+    pygame.mixer.init()
+    pygame.font.init()
+    pygame.display.set_caption("WindowName")
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
     clock = pygame.time.Clock()
 
+    # Variables
+
+    # Load assets
+
     # Game loop
-    while run:
+    while True:
         # Gets input
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
-
-        # Game logic
+                pygame.quit()
 
         # Updates screen
         pygame.display.update()
         clock.tick(FPS)
 
-    pygame.quit()
 
-
-main()
+if __name__ == "__main__":
+    main()
